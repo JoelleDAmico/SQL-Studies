444919a8d288a261038b5f07264e5b86259eaa97
JoelleDAmico <70987991+JoelleDAmico@users.noreply.github.com>
2025-01-03 17:57:42 -0500

    my-solution.sql
    Joelle D'Amico's solution for Q1 LeetCode SQL50

---
+++ LeetCode/Easy/Recyclable and Low Fat Products/my-solution.sql
@@ -0,0 +1,3 @@
+# Write your MySQL query statement below
+# ids (identifications) of products that are low_fats == 'Y' & recycleable == 'Y'
+SELECT product_id FROM Products WHERE low_fats='Y' AND recyclable= 'Y';
